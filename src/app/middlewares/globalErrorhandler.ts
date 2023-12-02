3d143d2293ea3031db01ad823f5d93292e2f06a9
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-12-03 01:29:22 +0600

    request schema  error handel

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,21 +2,48 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+import { TErrorSources } from '../interface/error';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
 
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: '',
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const statusCode = 400;
+    const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+    return {
+      statusCode,
+      message: 'Validation error',
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const formatZodError = handleZodError(err);
+    statusCode = formatZodError?.statusCode;
+    message = formatZodError?.message;
+    errorSources = formatZodError?.errorSources;
+  }
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
